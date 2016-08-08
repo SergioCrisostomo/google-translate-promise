3497b87ae25d55531cad112bbecea2309415e671
Sergio Crisostomo <sergiosbox@gmail.com>
2016-08-08 10:32:45 +0100

    add translation tests

--- tests/test.js
+++ tests/test.js
@@ -6,24 +6,51 @@ const API = require('../index');
 
 describe('Google translate API tests:', () => {
 
-	it('should use the global default option values', () => {
-		const translate = new API();
-		assert.equal(translate.options.timeout, 5000);
-	});
+	describe('Option setting tests', () => {
 
-	it('should set new global default option values', () => {
-		const translateA = new API();
-		translateA.setGlobalDefaults({timeout: 1000});
-		const translateB = new API();
-		assert.equal(translateB.options.timeout, 1000);
-	});
+		it('should use the global default option values', () => {
+			const GoogleTranslate = new API();
+			assert.equal(GoogleTranslate.options.timeout, 5000);
+		});
 
-	it('should set new local default option values', () => {
-		const translateA = new API({timeout: 3000});
-		translateA.setGlobalDefaults({timeout: 1000});
-		const translateB = new API();
-		assert.equal(translateA.options.timeout, 3000);
-		assert.equal(translateB.options.timeout, 1000);
+		it('should set new global default option values', () => {
+			const GoogleTranslateA = new API();
+			GoogleTranslateA.setGlobalDefaults({timeout: 1000});
+			const GoogleTranslateB = new API();
+			assert.equal(GoogleTranslateB.options.timeout, 1000);
+		});
+
+		it('should set new local default option values', () => {
+			const GoogleTranslateA = new API({timeout: 3000});
+			GoogleTranslateA.setGlobalDefaults({timeout: 1000});
+			const GoogleTranslateB = new API();
+			assert.equal(GoogleTranslateA.options.timeout, 3000);
+			assert.equal(GoogleTranslateB.options.timeout, 1000);
+		});
+	});
+	describe('Translation tests', () => {
+
+		it('should translate "Hello world!" to swedish', (done) => {
+			const phrase = 'Hello world!';
+			const GoogleTranslate = new API();
+			GoogleTranslate.translate(phrase, 'sv', 'en').then(text => {
+				assert.equal(text, 'Hej världen!');
+				done();
+			}).catch(reason => {
+				console.log(reason);
+			});
+		});
+
+		it('should translate "Hello world!" to portuguese', (done) => {
+			const phrase = 'Hello world!';
+			const GoogleTranslate = new API();
+			GoogleTranslate.translate(phrase, 'pt', 'en').then(text => {
+				assert.equal(text, 'Olá Mundo!');
+				done();
+			}).catch(reason => {
+				console.log(reason);
+			});
+		});
 	});
 
 });
